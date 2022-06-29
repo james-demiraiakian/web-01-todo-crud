24c43d93bc6db28e016a02443abafef43349d144
James Demiraiakian <james.armen@gmail.com>
2022-06-29 10:01:08 -0700

    testing github CI for eslint rules re. console.log()

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import { getUser, signOut } from './services/auth-service.js';
-import { getAllTodos, createTodo, updateTodo, deleteTodo } from './services/todo-service.js';
+import {
+    getAllTodos,
+    createTodo,
+    updateTodo,
+    deleteTodo,
+} from './services/todo-service.js';
 import { protectPage } from './utils.js';
 import createUser from './components/User.js';
 import createTodos from './components/Todos.js';
@@ -15,6 +20,7 @@ async function handlePageLoad() {
     protectPage(user);
 
     // *** set todos state from get all service function
+    console.log('user - in app.js', user);
 
     display();
 }
@@ -24,7 +30,7 @@ async function handleSignOut() {
 }
 
 async function handleAdd(task) {
-    // *** 
+    // ***
     // 1. create a new todo with description set to task and complete false
     // 2. push the new todo into the todos array
 
@@ -32,7 +38,7 @@ async function handleAdd(task) {
 }
 
 async function handleComplete(todo) {
-    // *** 
+    // ***
     // 1. Toggle todo complete property
     // 2. Get the index of the current todo
     // 3. Update that index of the array with the result of the update service function
@@ -61,16 +67,15 @@ async function handleDelete(todo) {
 const Todos = createTodos(document.querySelector('.todo-list'), {
     handleComplete,
     handleEdit,
-    handleDelete
+    handleDelete,
 });
 
-const AddTodo = createAddTodo(document.querySelector('.new-todo'), { handleAdd });
+const AddTodo = createAddTodo(document.querySelector('.new-todo'), {
+    handleAdd,
+});
 
-// Components 
-const User = createUser(
-    document.querySelector('#user'),
-    { handleSignOut }
-);
+// Components
+const User = createUser(document.querySelector('#user'), { handleSignOut });
 
 function display() {
     User({ user });
